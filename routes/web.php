0f70de9dd155798f30ddbc30acaab763779e91f9
Filipe Figueiredo <a13914@aedah.pt>
2020-10-12 17:27:17 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/toni', function () {
     return view('welcome');
+});
+
+Route::get('/bem-vindo', function () {
+    echo('olá mundo');
+});
+
+Route::get('/', function(){
+	echo('<h1>Olá mundo</h1>');
+});
+
+Route::get('/bem-vindo/aedah', function(){
+	echo('<h1>esta é para o Agrupamento :)</h1>');
+});
+
+Route::get('/nome/{nome}', function($nome){
+	echo('<h1>Olá '.$nome.'</h1>');
+});
+Route::get('/nome/{nome}/{apelido}', function($nome, $apelido){
+	echo('<h1>Olá '.$nome.' '.$apelido.'</h1>');
+});
+
+Route::get('/nomes/{nome}/{n}', function($nome, $n){
+	$numero=is_numeric($n)? $n:5;
+	for ($i=0; $i < $numero; $i++) { 
+		echo('<h1>Olá '.$nome.'</h1>');
+	};
+	return;
 });
